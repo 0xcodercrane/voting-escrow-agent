4ddb4d7efcf2a65877325d0ed2409b202df4934d
0xcodercrane <codercrane@gmail.com>
2022-07-04 17:51:16 +0800

    create deploy params

---
+++ src/config.ts
@@ -0,0 +1,10 @@
+export const DEPLOY_PARAMS = {
+  4: {
+    ve: "0x5Ba476927Ec72dF1B7717c0cc321797D95cb61B6",
+    feeDistributor: "0xef181de80ca41298c4031176f8b2c4268e0c9203",
+  },
+  1: {
+    ve: "0xD533a949740bb3306d119CC777fa900bA034cd52",
+    feeDistributor: "0xA464e6DCda8AC41e03616F95f4BC98a13b8922Dc",
+  },
+};
